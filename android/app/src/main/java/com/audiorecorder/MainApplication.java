2029fc8e7e7ae268fafbbd7ce2b0a5b845ca9b5d
TopTalentDev <toptal715@gmail.com>
2019-05-18 16:43:10 +0530

    link required modules

--- android/app/src/main/java/com/audiorecorder/MainApplication.java
+++ android/app/src/main/java/com/audiorecorder/MainApplication.java
@@ -3,6 +3,9 @@ package com.audiorecorder;
 import android.app.Application;
 
 import com.facebook.react.ReactApplication;
+import com.zmxv.RNSound.RNSoundPackage;
+import com.rnim.rn.audio.ReactNativeAudioPackage;
+import com.rnfs.RNFSPackage;
 import com.swmansion.gesturehandler.react.RNGestureHandlerPackage;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
@@ -24,6 +27,9 @@ public class MainApplication extends Application implements ReactApplication {
     protected List<ReactPackage> getPackages() {
       return Arrays.<ReactPackage>asList(
           new MainReactPackage(),
+          new RNSoundPackage(),
+          new ReactNativeAudioPackage(),
+          new RNFSPackage(),
           new RNGestureHandlerPackage()
       );
     }
